9f930d9d79864a133fc2413f77bab86b6fc58b1d
ryotarofr <ryoryo.fr0608@icloud.com>
2022-09-01 03:11:28 +0900

    ♻️

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,12 +2,12 @@ import BlogTemplateCard from "./BlogTemplateCard";
 import { Header } from "./Header";
 import { HomeTabs } from "./HomeTabs";
 import { SideMenuLayout } from "./sideMenu/SideMenuLayout";
-import { Navbar } from "./Navigation/Navbar";
+import { NavBar } from "./Navigation/NaBbar";
 export const Layout = () => {
   return (
     <>
       <Header />
-      <Navbar />
+      <NavBar />
       <div className="bg-gray-50">
         <HomeTabs />
         <div className="w-11/12 flex justify-between p-10 m-auto">
