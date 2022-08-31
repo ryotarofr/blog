9b1477644edbd85e23f8b43a546cabd7c3a735b7
ryotarofr <ryoryo.fr0608@icloud.com>
2022-08-31 20:54:57 +0900

    ♻️ uiを変更

--- components/Navigation/SearchBar.jsx
+++ components/Navigation/SearchBar.jsx
@@ -13,11 +13,23 @@ export const SearchBar = () => {
   return (
     <>
       {/* SearchBar */}
+
+      <svg
+        className="absolute text-slate-400 pl-2 h-9 w-7"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          clipRule="evenodd"
+        />
+      </svg>
       <input
         onClick={handleClick}
         ref={inputRef}
-        className="px-2 order-solid border-2 rounded-lg border-slate-300"
-        placeholder="Search"
+        className="py-1 pr-2 pl-6 order-solid border-2 rounded-lg border-slate-300"
+        placeholder="  Search"
       />
       {/* SearchBar Inner */}
       <SearchBarModal />
