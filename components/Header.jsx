803802c681f55d78eed3027771edfddcb5d239c8
ryotarofr <ryoryo.fr0608@icloud.com>
2022-08-31 20:52:35 +0900

    🎨 Headerコンポーネントを作成

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <>
+      <div className="flex justify-center pt-6 pb-4 text-2xl italic">
+        Fujiiの技術ブログ　
+        <p className="font-serif text-sm pt-3">Written by Ryotaro Fujii</p>
+      </div>
+    </>
+  );
+};
