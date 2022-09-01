d130825944fc3ad2cb103b81ec19f0f403affece
ryotarofr <ryoryo.fr0608@icloud.com>
2022-09-01 15:41:08 +0900

    🎨 NavBarのui変更

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,44 +1,18 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
-
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-
-import { BurgerList } from "./BurgerList";
+import { NavBarMenu } from "./NavBar-Menu";
 import { SearchBar } from "./SearchBar";
+import { BurgerList } from "./BurgerList";
 
 export const NavBar = () => {
-  const [value, setValue] = useState(0);
-  const router = useRouter();
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
   return (
-    <Box sx={{ borderBottom: 1, borderColor: "divider" }} className="">
+    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
       <nav className="container flex justify-around py-5 mx-auto">
         <div className="flex items-center">
           <h3 className="text-2xl font-medium text-blue-500">LOGO</h3>
         </div>
         {/* left header section */}
-        <div className="items-center hidden space-x-8 lg:flex">
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-          >
-            {/* 随時追加 */}
-            <Tab label="Home" onClick={() => router.push("/")} />
-            <Tab label="React" />
-            <Tab label="Python" />
-            <Tab label="その他" />
-            <Tab
-              value={4}
-              label="サイトマップ"
-              onClick={() => router.push("/sitemap")}
-            />
-          </Tabs>
+        <div className="items-center hidden lg:flex">
+          <NavBarMenu />
         </div>
 
         <div className="pt-2">
