4dd7b6941c844ae34133c86708af2fdb03a86043
ryotarofr <ryoryo.fr0608@icloud.com>
2022-09-04 15:24:23 +0900

    amplify

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,94 +1,98 @@
-// pages/index.js
-import { Authenticator } from "@aws-amplify/ui-react";
-import { Amplify, API, Auth, withSSRContext } from "aws-amplify";
+// // pages/index.js
+// import { Authenticator } from "@aws-amplify/ui-react";
+// import { Amplify, API, Auth, withSSRContext } from "aws-amplify";
 
-import awsExports from "../src/aws-exports";
-import { createPost } from "../src/graphql/mutations";
-import { listPosts } from "../src/graphql/queries";
+// import awsExports from "../src/aws-exports";
+// import { createPost } from "../src/graphql/mutations";
+// import { listPosts } from "../src/graphql/queries";
 
-Amplify.configure({ ...awsExports, ssr: true });
+// Amplify.configure({ ...awsExports, ssr: true });
 
-export async function getServerSideProps({ req }) {
-  const SSR = withSSRContext({ req });
-  const response = await SSR.API.graphql({ query: listPosts });
+// export async function getServerSideProps({ req }) {
+//   const SSR = withSSRContext({ req });
+//   const response = await SSR.API.graphql({ query: listPosts });
 
-  return {
-    props: {
-      posts: response.data.listPosts.items,
-    },
-  };
-}
+//   return {
+//     props: {
+//       posts: response.data.listPosts.items,
+//     },
+//   };
+// }
 
-async function handleCreatePost(event) {
-  event.preventDefault();
+// async function handleCreatePost(event) {
+//   event.preventDefault();
 
-  const form = new FormData(event.target);
+//   const form = new FormData(event.target);
 
-  try {
-    const { data } = await API.graphql({
-      authMode: "AMAZON_COGNITO_USER_POOLS",
-      query: createPost,
-      variables: {
-        input: {
-          title: form.get("title"),
-        },
-      },
-    });
+//   try {
+//     const { data } = await API.graphql({
+//       authMode: "AMAZON_COGNITO_USER_POOLS",
+//       query: createPost,
+//       variables: {
+//         input: {
+//           title: form.get("title"),
+//         },
+//       },
+//     });
 
-    window.location.href = `/posts/${data.createPost.id}`;
-  } catch ({ errors }) {
-    console.error(...errors);
-    throw new Error(errors[0].message);
-  }
-}
+//     window.location.href = `/posts/${data.createPost.id}`;
+//   } catch ({ errors }) {
+//     console.error(...errors);
+//     throw new Error(errors[0].message);
+//   }
+// }
 
-export default function Home({ posts = [] }) {
-  return (
-    <div>
-      <main>
-        <p>
-          <code>{posts.length}</code>
-          posts
-        </p>
+// export default function Home({ posts = [] }) {
+//   return (
+//     <div>
+//       <main>
+//         <p>
+//           <code>{posts.length}</code>
+//           posts
+//         </p>
 
-        <div>
-          {posts.map((post) => (
-            <a href={`/posts/${post.id}`} key={post.id}>
-              <h3>{post.title}</h3>
-              <p>{post.content}</p>
-            </a>
-          ))}
+//         <div>
+//           {posts.map((post) => (
+//             <a href={`/posts/${post.id}`} key={post.id}>
+//               <h3>{post.title}</h3>
+//               <p>{post.content}</p>
+//             </a>
+//           ))}
 
-          <div>
-            <h3>New Post</h3>
+//           <div>
+//             <h3>New Post</h3>
 
-            <Authenticator>
-              <form onSubmit={handleCreatePost}>
-                <fieldset>
-                  <legend>Title</legend>
-                  <input
-                    defaultValue={`Today, ${new Date().toLocaleTimeString()}`}
-                    name="title"
-                  />
-                </fieldset>
+//             <Authenticator>
+//               <form onSubmit={handleCreatePost}>
+//                 <fieldset>
+//                   <legend>Title</legend>
+//                   <input
+//                     defaultValue={`Today, ${new Date().toLocaleTimeString()}`}
+//                     name="title"
+//                   />
+//                 </fieldset>
 
-                <fieldset>
-                  <legend>Content</legend>
-                  <textarea
-                    defaultValue="I built an Amplify app with Next.js!"
-                    name="content"
-                  />
-                </fieldset>
+//                 <fieldset>
+//                   <legend>Content</legend>
+//                   <textarea
+//                     defaultValue="I built an Amplify app with Next.js!"
+//                     name="content"
+//                   />
+//                 </fieldset>
 
-                <button>Create Post</button>
-                <button type="button" onClick={() => Auth.signOut()}>
-                  Sign out
-                </button>
-              </form>
-            </Authenticator>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+//                 <button>Create Post</button>
+//                 <button type="button" onClick={() => Auth.signOut()}>
+//                   Sign out
+//                 </button>
+//               </form>
+//             </Authenticator>
+//           </div>
+//         </div>
+//       </main>
+//     </div>
+//   );
+// }
+
+export default function Home() {
+  return <></>;
 }
