0dc086f2ab2848bbc8434a3fcfb4a45f17995753
ryotarofr <ryoryo.fr0608@icloud.com>
2022-09-05 14:24:56 +0900

    delete folder

--- src/components/SearchBar-SelectButton.jsx
+++
@@ -1,7 +0,0 @@
-import { NativeSelect } from "@mantine/core";
-
-export const SearchBarSelectButton = () => {
-  return (
-    <NativeSelect data={["React", "Vue", "Angular", "Svelte"]} radius="md" />
-  );
-};
