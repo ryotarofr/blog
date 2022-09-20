60c9fd9d63509c3217cd31e1c08bbf87fe26c43b
ryotarofr <ryoryo.fr0608@icloud.com>
2022-09-20 23:51:29 +0900

    save data

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,7 +5,6 @@ import { Header } from "../components/Header";
 import { CloseButtonProvider } from "../context/CloseButtonContext";
 import { MediaQueryProvider } from "../context/MediaQueryContext";
 import { BlogTemplateProvider } from "../context/BlogTemplateContext";
-import { Layout } from "../components/Layout";
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- pages/create-post.jsx
+++ pages/create-post.jsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/router";
 
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
-import { Tiptap } from "../components/Editor";
 import { createPost } from "../src/graphql/mutations";
 
 const initialState = { title: "", content: "" };
